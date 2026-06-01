90c0149aadf3981cce6e331e04441300967dbb91
unknown <122757320+martamc00@users.noreply.github.com>
2026-06-01 22:09:10 +0100

    Animations: Scroll to Section & Scroll Reveal

---
+++ Landing-Page/script.js
@@ -0,0 +1,38 @@
+// Scroll to Sections
+
+const menuLinks = document.querySelectorAll('.menu-links a[href^="#"]');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({ 
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+            });
+});
+
+
+
+// SCROLLING ANIMATION
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible')
+        }
+    });
+}, { threshold: 0.1 });
+
+const animacaoElements = document.querySelectorAll('.fade-in');
+animacaoElements.forEach(elemento => {
+    observer.observe(elemento);
+});
