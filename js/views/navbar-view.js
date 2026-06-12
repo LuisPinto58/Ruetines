048a1f2c29363b7d2c2350ae9cc5e5df04ffb321
Marta Cardoso <122757320+martamc00@users.noreply.github.com>
2026-06-12 23:50:08 +0100

    updated colors on nav-bars

--- js/views/navbar-view.js
+++ js/views/navbar-view.js
@@ -19,18 +19,24 @@ function createDesktopNavbar(navItems) {
     const link = document.createElement('a');
     link.href = item.href || '#';
     link.textContent = item.label;
+
     if (item.href === 'nan') {
       link.addEventListener('click', function (event) {
         event.preventDefault();
         alert('Funcionalidade não disponível para utilizadores não autenticados!');
       });
       link.classList.add('disabled');
-    } if (item.href === window.location.pathname.replace("/Ruetines", "..")) {
-      link.classList.add('active');
+    } else {
+      const fileName = item.href.split('/').pop();
+      if (fileName && window.location.pathname.includes(fileName)) {
+        link.classList.add('active');
+      }
     }
+
     li.appendChild(link);
     navList.appendChild(li);
   });
+
   if (User.fromStorage()) {
     navbar.appendChild(logoContainer);
     navbar.appendChild(navList);
@@ -111,6 +117,12 @@ function createMobileNavbar(navItems) {
         event.preventDefault();
         alert('Funcionalidade não disponível para utilizadores não autenticados!');
       });
+      link.classList.add('disabled');    
+    } else {
+      const fileName = item.href.split('/').pop();
+      if (fileName && window.location.pathname.includes(fileName)) {
+        link.classList.add('active');
+      }
     }
     link.title = item.label;
 
@@ -174,17 +186,17 @@ function createLoginModal() {
 }
 
 function getNavItems() {
-    const userIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6b5fa0" class="bi bi-person"
+    const userIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person"
                 viewBox="0 0 16 16">
                 <path
                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
             </svg>`
-  const taskIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6b5fa0" class="bi bi-list-task" viewBox="0 0 16 16">
+  const taskIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list-task" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5zM3 3H2v1h1z"/>
               <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1z"/>
               <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z"/>
             </svg>`
-  const chatIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="margin-left:0.5rem" fill="#6b5fa0" class="bi bi-chat" viewBox="0 0 16 16">
+  const chatIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="margin-left:0.5rem" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
                 <path  d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
             </svg>`
 
