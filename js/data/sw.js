9a5ecbb52165b3323884e498331921f8189d7b09
LP1201083 <40230258@esmad.ipp.pt>
2026-06-14 10:23:50 +0100

    added sw for offline functionality and manifest

---
+++ js/data/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "ruetines-v2";
+const APP_SHELL = [
+  "../html/tasks.html",
+  "../css/index.css",
+  "../css/tasks.css",
+  "../css/components.css",
+  "../js/views/navbar-view.js",
+  "../js/views/tasks-view.js",
+  "../js/controller/tasks-controller.js",
+  "../js/models/tasks-model.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .catch(() => caches.match("../html/tasks.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match("../html/tasks.html"));
+    })
+  );
+});
